50b78b817098e3727fd3da19aca57b8f98f7a9dc
Fabian Ruff <fabian.ruff@sap.com>
2017-07-05 11:48:50 +0200

    move client creation to kube package

--- pkg/api/rest/kubeclient.go
+++ pkg/api/rest/kubeclient.go
@@ -1,7 +1,6 @@
 package rest
 
 import (
-	"github.com/golang/glog"
 	"github.com/spf13/pflag"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
@@ -17,23 +16,5 @@ func init() {
 }
 
 func NewKubeClient() (*kubernetes.Clientset, *rest.RESTClient, *runtime.Scheme) {
-	config := kube.NewClientConfig(kube.Options{ConfigFile: kubeconfig})
-	clientset, err := kube.NewClient(config)
-	if err != nil {
-		glog.Fatalf("Failed to create kubernetes clientset: %s", err)
-	}
-	if err := kube.EnsureTPR(clientset); err != nil {
-		glog.Fatalf("Failed to create TPR: %s", err)
-	}
-
-	tprClient, tprScheme, err := kube.NewTPRClient(config)
-	if err != nil {
-		glog.Fatalf("Failed to create TPR client: %s", err)
-	}
-
-	glog.V(3).Info("Waiting for TPR resource to become available")
-	if err := kube.WaitForTPR(tprClient); err != nil {
-		glog.Fatalf("TPR resource unavailable: %s", err)
-	}
-	return clientset, tprClient, tprScheme
+	return kube.NewClients(kube.Options{ConfigFile: kubeconfig})
 }
--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -2,6 +2,7 @@ package kube
 
 import (
 	"github.com/golang/glog"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -11,7 +12,29 @@ type Options struct {
 	ConfigFile string
 }
 
-func NewClient(config *rest.Config) (*kubernetes.Clientset, error) {
+func NewClients(options Options) (*kubernetes.Clientset, *rest.RESTClient, *runtime.Scheme) {
+	config := NewClientConfig(options)
+	clientset, err := NewClientset(config)
+	if err != nil {
+		glog.Fatalf("Failed to create kubernetes clientset: %s", err)
+	}
+	if err := EnsureTPR(clientset); err != nil {
+		glog.Fatalf("Failed to create TPR: %s", err)
+	}
+
+	tprClient, tprScheme, err := NewTPRClient(config)
+	if err != nil {
+		glog.Fatalf("Failed to create TPR client: %s", err)
+	}
+
+	glog.V(3).Info("Waiting for TPR resource to become available")
+	if err := WaitForTPR(tprClient); err != nil {
+		glog.Fatalf("TPR resource unavailable: %s", err)
+	}
+	return clientset, tprClient, tprScheme
+}
+
+func NewClientset(config *rest.Config) (*kubernetes.Clientset, error) {
 	client, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		return nil, err
