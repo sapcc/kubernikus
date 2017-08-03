1e118dce99a765f5f2a315bdd52bdd5c033bbb81
Arno Uhlig <arno.uhlig@sap.com>
2017-08-03 15:52:08 +0200

    patch kluster state

--- pkg/api/handlers/terminate_cluster.go
+++ pkg/api/handlers/terminate_cluster.go
@@ -5,9 +5,10 @@ import (
 	"github.com/sapcc/kubernikus/pkg/api"
 	"github.com/sapcc/kubernikus/pkg/api/models"
 	"github.com/sapcc/kubernikus/pkg/api/rest/operations"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	tprv1 "github.com/sapcc/kubernikus/pkg/tpr/v1"
+	"github.com/golang/glog"
 )
 
 func NewTerminateCluster(rt *api.Runtime) operations.TerminateClusterHandler {
@@ -19,11 +20,28 @@ type terminateCluster struct {
 }
 
 func (d *terminateCluster) Handle(params operations.TerminateClusterParams, principal *models.Principal) middleware.Responder {
-
-	if err := d.rt.Clients.TPRClient().Delete().Namespace("kubernikus").Resource(tprv1.KlusterResourcePlural).LabelsSelectorParam(accountSelector(principal)).Name(params.Name).Do().Error(); err != nil {
+	var oldKluster *tprv1.Kluster
+	if err := d.rt.Clients.TPRClient().Get().Namespace("kubernikus").Resource(tprv1.KlusterResourcePlural).LabelsSelectorParam(accountSelector(principal)).Name(params.Name).Do().Into(oldKluster); err != nil {
 		if apierrors.IsNotFound(err) {
 			return operations.NewTerminateClusterDefault(404).WithPayload(modelsError(err))
 		}
+		return operations.NewTerminateClusterDefault(0).WithPayload(modelsError(err))
+	}
+
+	copy, err := d.rt.Clients.TPRScheme().Copy(oldKluster)
+	if err != nil {
+
+	}
+	cpKluster := copy.(*tprv1.Kluster)
+	cpKluster.Status.State = tprv1.KlusterTerminating
+
+	patchBytes, patchType, err := createPatch(oldKluster,cpKluster)
+	if err != nil {
+		return operations.NewTerminateClusterDefault(0).WithPayload(modelsError(err))
+	}
+
+	if err := d.rt.Clients.TPRClient().Patch(patchType).Body(patchBytes).Do().Error(); err != nil {
+		glog.Errorf("Failed to patch %s/%s: %s",oldKluster.GetNamespace(),oldKluster.GetName(),err)
 		return operations.NewTerminateClusterDefault(0).WithPayload(modelsError(err))
 	}
 
--- pkg/api/handlers/util.go
+++ pkg/api/handlers/util.go
@@ -1,9 +1,15 @@
 package handlers
 
 import (
+	"encoding/json"
+
 	"github.com/go-openapi/swag"
 	"github.com/sapcc/kubernikus/pkg/api/models"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/strategicpatch"
+
+	tprv1 "github.com/sapcc/kubernikus/pkg/tpr/v1"
 )
 
 func modelsError(err error) *models.Error {
@@ -14,4 +20,25 @@ func modelsError(err error) *models.Error {
 
 func accountSelector(principal *models.Principal) labels.Selector {
 	return labels.SelectorFromSet(map[string]string{"account": principal.Account})
+}
+
+func createPatch(old, new *tprv1.Kluster) (patchBytes []byte, patchType types.PatchType, err error) {
+
+	oldData, err := json.Marshal(old)
+	if err != nil {
+		return nil, types.StrategicMergePatchType, err
+	}
+
+	newData, err := json.Marshal(new)
+	if err != nil {
+		return nil, types.StrategicMergePatchType, err
+	}
+
+	patchBytes, err = strategicpatch.CreateTwoWayMergePatch(oldData, newData, tprv1.Kluster{})
+	if err != nil {
+		return nil, types.StrategicMergePatchType, err
+	}
+
+	return patchBytes, types.StrategicMergePatchType, nil
+
 }
