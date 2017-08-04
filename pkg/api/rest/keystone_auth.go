1407af602187eec0bdd807b0314cc919b1ca7f8c
Fabian Ruff <fabian.ruff@sap.com>
2017-08-04 10:11:48 +0200

    Append /v3 to auth aurl if not found

--- pkg/api/rest/keystone_auth.go
+++ pkg/api/rest/keystone_auth.go
@@ -1,6 +1,9 @@
 package rest
 
 import (
+	"fmt"
+	"path"
+	"strings"
 	"time"
 
 	"github.com/databus23/keystone"
@@ -19,13 +22,17 @@ func init() {
 
 func keystoneAuth() func(token string) (*models.Principal, error) {
 
+	if !(strings.HasSuffix("/v3") || strings.HasSuffix("/v3/")) {
+		authURL = path.Join(authURL, "/v3")
+	}
+
 	auth := keystone.New(authURL)
 	auth.TokenCache = memory.New(10 * time.Minute)
 
 	return func(token string) (*models.Principal, error) {
 		t, err := auth.Validate(token)
 		if err != nil {
-			return nil, errors.New(401, err.Error())
+			return nil, errors.New(401, fmt.Sprintf("Authentication failed: %s", err))
 		}
 		if t.Project == nil {
 			return nil, errors.New(401, "Auth token isn't project scoped")
