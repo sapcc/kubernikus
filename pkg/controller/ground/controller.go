fb9f9b04b405413c24843dd98858669b13bae5ba
Fabian Ruff <fabian.ruff@sap.com>
2017-08-28 13:32:08 +0200

    Revert "loosen update trigger"
    This reverts commit 8b023b87653e45e289b334d12e83eb84dbcf9b17.

--- pkg/controller/ground/controller.go
+++ pkg/controller/ground/controller.go
@@ -216,7 +216,7 @@ func (op *Operator) klusterTerminate(obj interface{}) {
 func (op *Operator) klusterUpdate(cur, old interface{}) {
 	curKluster := cur.(*tprv1.Kluster)
 	oldKluster := old.(*tprv1.Kluster)
-	if !reflect.DeepEqual(oldKluster, curKluster) {
+	if !reflect.DeepEqual(oldKluster.Spec, curKluster.Spec) {
 		key, err := cache.MetaNamespaceIndexFunc(curKluster)
 		if err != nil {
 			return
