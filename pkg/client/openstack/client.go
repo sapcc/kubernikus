afacd21c78046952dd31c0869992cd5fd04d209a
Michael Schmidt <michael.j.schmidt@gmail.com>
2017-09-18 11:08:16 +0200

    this state function

--- pkg/client/openstack/client.go
+++ pkg/client/openstack/client.go
@@ -79,23 +79,46 @@ type Node struct {
 	StateExt
 }
 
-func (n *Node) Ready() bool {
-	glog.V(6).Infof("[%v] PowerState: %v, VMState: %v, TaskState: %v", n.Name, n.PowerState, n.VMState, n.TaskState)
+func (n *Node) Starting() bool {
+	// https://github.com/openstack/nova/blob/be3a66781f7fd58e5c5c0fe89b33f8098cfb0f0d/nova/objects/fields.py#L884
+	if n.TaskState == "spawning" || n.TaskState == "scheduling" || n.TaskState == "networking" || n.TaskState == "block_device_mapping" {
+		return true
+	}
+
+	if n.TaskState != "" {
+		return false
+	}
+
+	if n.VMState == "building" {
+		return true
+	}
+
+	return false
+}
+
+func (n *Node) Stopping() bool {
+	if n.TaskState == "spawning" || n.TaskState == "scheduling" || n.TaskState == "networking" || n.TaskState == "block_device_mapping" {
+		return false
+	}
+
+	if n.TaskState != "" {
+		return true
+	}
+
+	return false
+}
+
+func (n *Node) Running() bool {
+	if n.Starting() || n.Stopping() {
+		return false
+	}
+
 	// 0: NOSTATE
 	// 1: RUNNING
 	// 3: PAUSED
 	// 4: SHUTDOWN
 	// 6: CRASHED
 	// 7: SUSPENDED
-	if n.TaskState == "spawning" || n.TaskState == "scheduling" || n.TaskState == "networking" || n.TaskState == "block_device_mapping" {
-		return true
-	}
-
-	// https://github.com/openstack/nova/blob/be3a66781f7fd58e5c5c0fe89b33f8098cfb0f0d/nova/objects/fields.py#L884
-	if n.TaskState == "deleting" {
-		return false
-	}
-
 	if n.PowerState > 1 {
 		return false
 	}
@@ -112,12 +135,11 @@ func (n *Node) Ready() bool {
 	//ERROR = 'error'
 	//SHELVED = 'shelved'
 	//SHELVED_OFFLOADED = 'shelved_offloaded'
-
-	if !(n.VMState == "active" || n.VMState == "building") {
-		return false
+	if n.VMState == "active" {
+		return true
 	}
 
-	return true
+	return false
 }
 
 type StateExt struct {
