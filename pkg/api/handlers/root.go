3f52dcf20f4d1f378cc1f17a8bd550024726b605
Michael Schmidt <michael.j.schmidt@gmail.com>
2017-10-26 16:23:48 +0200

    redirects to /docs when text/html to api-info otherwise

--- pkg/api/handlers/root.go
+++ pkg/api/handlers/root.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 var RootContent = `{
   "paths": [
@@ -16,6 +19,13 @@ var RootContent = `{
 func RootHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
+			for _, accept := range strings.Split(r.Header.Get("Accept"), ",") {
+				switch strings.TrimSpace(accept) {
+				case "text/html":
+					http.Redirect(rw, r, "/docs", 301)
+					return
+				}
+			}
 			rw.Write([]byte(RootContent))
 		} else {
 			next.ServeHTTP(rw, r)
