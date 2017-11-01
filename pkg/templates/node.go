457f879c9f96ffd926db3b2a63dce4f71eeb5a0e
Fabian Ruff <fabian.ruff@sap.com>
2017-11-01 16:42:41 +0100

    pass listen address to wormhole unit file

--- pkg/templates/node.go
+++ pkg/templates/node.go
@@ -103,7 +103,7 @@ systemd:
           --volume etc-kubernetes-certs,kind=host,source=/etc/kubernetes/certs,readOnly=true \
           --mount volume=etc-kubernetes-certs,target=/etc/kubernetes/certs \
           docker://{{ .KubernikusImage }}:{{ .KubernikusImageTag }} \
-          --exec wormhole -- client --kubeconfig=/var/lib/kubelet/kubeconfig
+          --exec wormhole -- client --listen {{ .ApiserverIP }}:6443 --kubeconfig=/var/lib/kubelet/kubeconfig
         ExecStopPost=/usr/bin/rkt gc --mark-only
         KillMode=mixed
         Restart=always
