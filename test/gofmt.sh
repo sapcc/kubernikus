93b015e7f788cd1cdfbaba77ff35066297cf8773
Fabian Ruff <fabian.ruff@sap.com>
2017-12-15 13:13:55 +0100

    Improve output when format check failes
    [ci skip]

--- test/gofmt.sh
+++ test/gofmt.sh
@@ -3,7 +3,9 @@ set -eo pipefail
 
 VIOLATING_FILES=$(goimports -local github.com/sapcc/kubernikus -l $@ | sed /generated/d)
 if [ -n "$VIOLATING_FILES" ]; then
-  echo "Go code is not formatted:"
-  goimports -e -d $@
+  echo "Go code is not formatted in these files:"
+  echo "$VIOLATING_FILES"
+  echo "Offending lines:"
+  goimports -local github.com/sapcc/kubernikus -e -d $VIOLATING_FILES
   exit 1
 fi
