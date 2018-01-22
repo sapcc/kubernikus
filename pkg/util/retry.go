5ee885dac7d850a45e1c273a61b930ed0c3d38e9
Fabian Ruff <fabian.ruff@sap.com>
2018-01-22 14:56:34 +0100

    Retry kluster update operations
    This PR adds a util.UpdateKlusterWithRetries method that uses the local cache to retry an update to the Kluster CRD 4 times before it fails. It also changes to phase change to a PATCH operation that should be more robust against write conflicts.

---
+++ pkg/util/retry.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"errors"
+
+	"k8s.io/client-go/util/retry"
+
+	"github.com/sapcc/kubernikus/pkg/apis/kubernikus/v1"
+	client "github.com/sapcc/kubernikus/pkg/generated/clientset/typed/kubernikus/v1"
+	listers_kubernikus "github.com/sapcc/kubernikus/pkg/generated/listers/kubernikus/v1"
+)
+
+type updateKlusterFunc func(kluster *v1.Kluster) error
+
+var KlusterNotUpdated = errors.New("Kluster not updated")
+
+// UpdateKlusterWithRetries updates a kluster with given applyUpdate function.
+func UpdateKlusterWithRetries(klusterClient client.KlusterInterface, klusterLister listers_kubernikus.KlusterNamespaceLister, name string, applyUpdate updateKlusterFunc) (*v1.Kluster, error) {
+	var kluster *v1.Kluster
+
+	retryErr := retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+		var err error
+		kluster, err = klusterLister.Get(name)
+		if err != nil {
+			return err
+		}
+		kluster = kluster.DeepCopy()
+		// Apply the update, then attempt to push it to the apiserver.
+		if applyErr := applyUpdate(kluster); applyErr != nil {
+			if err == KlusterNotUpdated {
+				return nil
+			}
+			return applyErr
+		}
+		kluster, err = klusterClient.Update(kluster)
+		return err
+	})
+
+	return kluster, retryErr
+}
