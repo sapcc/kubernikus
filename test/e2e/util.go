6c15c6ae20a9819edcdf0332a124657f2f64a6ff
Michael Schmidt <michael.j.schmidt@gmail.com>
2018-02-20 16:28:56 +0100

    adds ssh key to e2e cluster

--- test/e2e/util.go
+++ test/e2e/util.go
@@ -130,6 +130,7 @@ func newE2ECluster(klusterName string) *models.Kluster {
 	return &models.Kluster{
 		Name: klusterName,
 		Spec: models.KlusterSpec{
+			SSHPublicKey: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCXIxVEUgtUVkvk2VM1hmIb8MxvxsmvYoiq9OBy3J8akTGNybqKsA2uhcwxSJX5Cn3si8kfMfka9EWiJT+e1ybvtsGILO5XRZPxyhYzexwb3TcALwc3LuzpF3Z/Dg2jYTRELTGhYmyca3mxzTlCjNXvYayLNedjJ8fIBzoCuSXNqDRToHru7h0Glz+wtuE74mNkOiXSvhtuJtJs7VCNVjobFQNfC1aeDsri2bPRHJJZJ0QF4LLYSayMEz3lVwIDyAviQR2Aa97WfuXiofiAemfGqiH47Kq6b8X7j3bOYGBvJKMUV7XeWhGsskAmTsvvnFxkc5PAD3Ct+liULjiQWlzDrmpTE8aMqLK4l0YQw7/8iRVz6gli42iEc2ZG56ob1ErpTLAKFWyCNOebZuGoygdEQaGTIIunAncXg5Rz07TdPl0Tf5ZZLpiAgR5ck0H1SETnjDTZ/S83CiVZWJgmCpu8YOKWyYRD4orWwdnA77L4+ixeojLIhEoNL8KlBgsP9Twx+fFMWLfxMmiuX+yksM6Hu+Lsm+Ao7Q284VPp36EB1rxP1JM7HCiEOEm50Jb6hNKjgN4aoLhG5yg+GnDhwCZqUwcRJo1bWtm3QvRA+rzrGZkId4EY3cyOK5QnYV5+24x93Ex0UspHMn7HGsHUESsVeV0fLqlfXyd2RbHTmDMP6w== Kubernikus Master Key",
 			NodePools: []models.NodePool{
 				{
 					Name:   "small",
@@ -172,8 +173,6 @@ func newE2ESmokeTestCluster(klusterName string) *models.Kluster {
 		},
 	}
 }
-
-
 
 func isPodRunning(event watch.Event) (bool, error) {
 	switch event.Type {
