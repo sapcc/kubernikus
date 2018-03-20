32c6f6810250b55b5278d3dc8588160524e24b95
Fabian Ruff <fabian@progra.de>
2018-03-20 13:08:40 +0100

    Add some safe gurad comments about adding migration steps

--- pkg/migration/register.go
+++ pkg/migration/register.go
@@ -1,7 +1,13 @@
 package migration
 
+// In this function we register the migrations for the Kluster CRD
+// IMPORTANT: Don't remove migrations, don't reorder them!
+// The position in the migrations slice is used for versioning,
+// so the only thing that is sane is to append migrations
+// to the end of the slice
 func init() {
 	defaultRegistry.migrations = []Migration{
 		Init,
+		// <-- Insert new migrations at the end only!
 	}
 }
