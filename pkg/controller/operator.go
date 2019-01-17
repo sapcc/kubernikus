0e19af9003ab9331be3634cd21c6c7d03d237e90
Michael Schmidt <michael.j.schmidt@gmail.com>
2019-01-17 10:53:09 +0100

    adds more launchctl workers

--- pkg/controller/operator.go
+++ pkg/controller/operator.go
@@ -172,7 +172,7 @@ func NewKubernikusOperator(options *KubernikusOperatorOptions, logger log.Logger
 		case "groundctl":
 			o.Config.Kubernikus.Controllers["groundctl"] = NewGroundController(10, o.Factories, o.Clients, recorder, o.Config, logger)
 		case "launchctl":
-			o.Config.Kubernikus.Controllers["launchctl"] = launch.NewController(1, o.Factories, o.Clients, recorder, logger)
+			o.Config.Kubernikus.Controllers["launchctl"] = launch.NewController(10, o.Factories, o.Clients, recorder, logger)
 		case "routegc":
 			o.Config.Kubernikus.Controllers["routegc"] = routegc.New(60*time.Second, o.Factories, logger)
 		case "deorbiter":
