b401af4f131d4b70e99d29a189b574b14fcc7002
Fabian Ruff <fabian@progra.de>
2019-01-24 11:42:56 +0100

    Revert migration version 3 to previous state

--- pkg/migration/3_etcdbr_create_storage_container.go
+++ pkg/migration/3_etcdbr_create_storage_container.go
@@ -1,18 +1,30 @@
 package migration
 
 import (
+	"errors"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
-	v1 "github.com/sapcc/kubernikus/pkg/apis/kubernikus/v1"
+	"github.com/sapcc/kubernikus/pkg/apis/kubernikus/v1"
 	"github.com/sapcc/kubernikus/pkg/client/openstack"
-	"github.com/sapcc/kubernikus/pkg/util"
 	etcd_util "github.com/sapcc/kubernikus/pkg/util/etcd"
 )
 
 func CreateEtcdBackupStorageContainer(rawKluster []byte, current *v1.Kluster, client kubernetes.Interface, openstackFactory openstack.SharedOpenstackClientFactory) (err error) {
-	secret, err := util.KlusterSecret(client, current)
+	secret, err := client.CoreV1().Secrets(current.GetNamespace()).Get(current.GetName(), metav1.GetOptions{})
 	if err != nil {
 		return err
+	}
+
+	username, ok := secret.Data["openstack-username"]
+	if !ok {
+		return errors.New("openstack username secret not set")
+	}
+
+	domain, ok := secret.Data["openstack-domain-name"]
+	if !ok {
+		return errors.New("openstack domain name secret not set")
 	}
 
 	adminClient, err := openstackFactory.AdminClient()
@@ -23,8 +35,8 @@ func CreateEtcdBackupStorageContainer(rawKluster []byte, current *v1.Kluster, cl
 	err = adminClient.CreateStorageContainer(
 		current.Spec.Openstack.ProjectID,
 		etcd_util.DefaultStorageContainer(current),
-		secret.Openstack.Username,
-		secret.Openstack.DomainName,
+		string(username),
+		string(domain),
 	)
 
 	return err
