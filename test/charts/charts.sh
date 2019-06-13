f460d75e167cabc03cbb3de8f0b4d79644ddaf41
Arno Uhlig <arno.uhlig@sap.com>
2019-06-13 16:29:33 +0200

    drop api version test

--- test/charts/charts.sh
+++ test/charts/charts.sh
@@ -50,29 +50,5 @@ for chart in $pwd/charts/*; do
     fi
     cd ..
     echo "Done."
-
-    echo "Checking API versions ..."
-    while IFS= read -r line <&3; do
-      if echo "$line" | grep "^---$" > /dev/null; then
-        unset api_real kind_real
-        continue
-      fi
-      api_tmp=`echo $line | grep "^apiVersion: .*$" | awk -F': ' '{print $2}' | sed 's/\"//g'`
-      kind_tmp=`echo $line | grep "^kind: .*$" | awk -F': ' '{print $2}' | sed 's/\"//g'`
-      if [[ ! -z "$api_tmp" ]]; then
-        api_real=$api_tmp
-      fi
-      if [[ ! -z "$kind_tmp" ]]; then
-        kind_real=$kind_tmp
-      fi
-      if [[ ! -z "$api_real" && ! -z "$kind_real" ]]; then
-        if ! echo "$resources" | grep "^$kind_real:$api_real$" > /dev/null; then
-          echo "kind: $kind_real apiVersion: $api_real not matching minimum version requirements ($k8s_min_version)!"
-          exit 1
-        fi
-        unset api_real kind_real
-      fi      
-    done 3< "/tmp/chart.yaml"
-    echo "Done."
   fi
 done
