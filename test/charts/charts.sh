8e6986f2d6501abc2316b18c6d00bd3f37910b45
Arno Uhlig <arno.uhlig@sap.com>
2019-06-14 10:47:06 +0200

    removes unused parts

--- test/charts/charts.sh
+++ test/charts/charts.sh
@@ -1,26 +1,5 @@
 #!/bin/ash
 
-#set -o xtrace
-
-# get minimum supported api version with `kubectl explain resource`
-# in ash no arrays are supported, using grep on env var instead
-k8s_min_version="1.7"
-resources=`echo -e "ClusterRole:rbac.authorization.k8s.io/v1alpha1\n\
-ClusterRole:rbac.authorization.k8s.io/v1beta1\n\
-ClusterRoleBinding:rbac.authorization.k8s.io/v1beta1\n\
-ClusterRoleBinding:rbac.authorization.k8s.io/v1alpha1\n\
-Config:v1\n\
-ConfigMap:v1\n\
-DaemonSet:extensions/v1beta1\n\
-Deployment:extensions/v1beta1\n\
-Ingress:extensions/v1beta1\n\
-PersistentVolumeClaim:v1\n\
-Role:rbac.authorization.k8s.io/v1beta1\n\
-RoleBinding:rbac.authorization.k8s.io/v1beta1\n\
-Secret:v1\n\
-Service:v1\n\
-ServiceAccount:v1"`
-
 helm init --client-only
 helm repo add bugroger-charts https://raw.githubusercontent.com/BugRoger/charts/repo
 helm repo add sapcc https://charts.global.cloud.sap
