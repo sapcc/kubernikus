498df9f4fe4c7eb6a44904cc70ebd16b23412641
Jan Knipper <jan.knipper@sap.com>
2020-11-11 15:18:59 +0100

    Fix hammertime while upgrading

--- pkg/util/node.go
+++ pkg/util/node.go
@@ -12,6 +12,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 
+	"github.com/Masterminds/semver"
 	"github.com/sapcc/kubernikus/pkg/api/models"
 	"github.com/sapcc/kubernikus/pkg/util/netutil"
 )
@@ -148,4 +149,16 @@ func IsCoreOSNodePool(pool *models.NodePool) bool {
 
 func IsFlatcarNodePool(pool *models.NodePool) bool {
 	return pool.Image == NODEPOOL_FLATCAR_IMAGE
+}
+
+func NodeVersionConstraint(node *v1.Node, constraint string) (bool, error) {
+	c, err := semver.NewConstraint(constraint)
+	if err != nil {
+		return false, err
+	}
+	v, err := semver.NewVersion(node.Status.NodeInfo.KubeletVersion)
+	if err != nil {
+		return false, err
+	}
+	return c.Check(v), nil
 }
