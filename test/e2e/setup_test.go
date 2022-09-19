695c1a4e705c70657f28c103ecdc68fbb24314bb
Fabian Ruff <fabian.ruff@sap.com>
2022-09-19 10:09:22 +0200

    create 1.23.11 clusters in regional soak test

--- test/e2e/setup_test.go
+++ test/e2e/setup_test.go
@@ -40,7 +40,7 @@ func (s *SetupTests) Run(t *testing.T) {
 }
 
 func (s *SetupTests) CreateCluster(t *testing.T) {
-	version := "1.23.6"
+	version := "1.23.11"
 	if v := os.Getenv("KLUSTER_VERSION"); v != "" {
 		version = v
 	}
