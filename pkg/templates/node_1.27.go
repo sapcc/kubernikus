fab2229050edc7722d818e546079448a59718561
Jan Knipper <9881823+jknipper@users.noreply.github.com>
2024-03-12 10:32:38 +0100

    Only offer secure ciphers in kubelet server (#883)
    * Only offer TLS 1.3 in kubelet server
    
    * Add specific cipher list

--- pkg/templates/node_1.27.go
+++ pkg/templates/node_1.27.go
@@ -297,6 +297,14 @@ storage:
           rotateCertificates: true
           nodeLeaseDurationSeconds: 20
           cgroupDriver: systemd
+          tlsCipherSuites:
+          - TLS_CHACHA20_POLY1305_SHA256
+          - TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
+          - TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384
+          - TLS_AES_128_GCM_SHA256
+          - TLS_AES_256_GCM_SHA384
+          - TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
+          - TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384
     - path: /etc/flatcar/update.conf
       filesystem: root
       mode: 0644
