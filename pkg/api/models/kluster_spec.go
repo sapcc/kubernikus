7da2e7fd576eb54759a3d47394ba8a9cec4a8c3c
Jan Knipper <jan.knipper@sap.com>
2024-05-29 11:42:48 +0200

    Seed virtual kluster

--- pkg/api/models/kluster_spec.go
+++ pkg/api/models/kluster_spec.go
@@ -73,6 +73,9 @@ type KlusterSpec struct {
 	// seed kubeadm
 	SeedKubeadm bool `json:"seedKubeadm,omitempty"`
 
+	// seed virtual
+	SeedVirtual bool `json:"seedVirtual,omitempty"`
+
 	// CIDR Range for Services in the cluster. Can not be updated.
 	// Pattern: ^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$
 	ServiceCIDR string `json:"serviceCIDR,omitempty"`
