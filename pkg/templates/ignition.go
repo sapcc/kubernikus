1349a4d75a4cd5cd6f1f65d7a78a852a4259357d
Jan Knipper <9881823+jknipper@users.noreply.github.com>
2025-07-09 14:07:38 +0200

    Add template support for k8s 1.32/1.33 (#1020)

--- pkg/templates/ignition.go
+++ pkg/templates/ignition.go
@@ -30,6 +30,10 @@ const TEMPLATE_VERSION = "9"
 
 func (i *ignition) getIgnitionTemplate(kluster *kubernikusv1.Kluster) (string, error) {
 	switch {
+	case strings.HasPrefix(kluster.Spec.Version, "1.33"):
+		return Node_1_27, nil // No changes to 1.27
+	case strings.HasPrefix(kluster.Spec.Version, "1.32"):
+		return Node_1_27, nil // No changes to 1.27
 	case strings.HasPrefix(kluster.Spec.Version, "1.31"):
 		return Node_1_27, nil // No changes to 1.27
 	case strings.HasPrefix(kluster.Spec.Version, "1.30"):
