10dfb266f9e9321c0a61029002eda31d87bce472
Nata Tchikhinashvili <nata.tchikhinashvili@sap.com>
2026-06-10 13:54:46 +0200

    fix: weak umac algorithm

--- pkg/templates/node_1.27.go
+++ pkg/templates/node_1.27.go
@@ -124,6 +124,27 @@ storage:
       contents:
         inline: |
           runtime-endpoint: unix:///run/containerd/containerd.sock
+    - path: /etc/ssh/sshd_config
+      filesystem: root
+      mode: 0644
+      overwrite: true
+      contents:
+        inline: |
+          # Use most defaults for sshd configuration.
+          #Subsystem sftp internal-sftp
+          ClientAliveInterval 180
+          UseDNS no
+          UsePAM yes
+
+          # handled by PAM
+          PrintLastLog no
+
+          # handled by PAM
+          PrintMotd no
+
+          Ciphers chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com
+          MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha2-256,hmac-sha2-512,umac-128-etm@openssh.com,umac-128@openssh.com
+          KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-sha256
     - path: /etc/profile.d/envs.sh
       filesystem: root
       mode: 0644
