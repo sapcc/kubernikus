9f55de6bd811143c73f3d631993e1bf8dcee96cd
Nata Tchikhinashvili <nata.tchikhinashvili@sap.com>
2026-07-07 20:49:00 +0200

    update algorithm

--- pkg/templates/node_1.27.go
+++ pkg/templates/node_1.27.go
@@ -143,7 +143,7 @@ storage:
           PrintMotd no
 
           Ciphers chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com
-          MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha2-256,hmac-sha2-512,umac-128-etm@openssh.com,umac-128@openssh.com
+          MACs -umac-64*,hmac-sha1*
           KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-sha256
     - path: /etc/profile.d/envs.sh
       filesystem: root
